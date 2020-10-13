cd01db7d92fc41f013872f637d9c50cf99d518b2
Ella Lindstrom <ella.lindstroem.548@k12.friscoisd.org>
2020-10-13 09:10:01 -0500

    added scoreBox and green background

--- WhackAMole/ViewController.swift
+++ WhackAMole/ViewController.swift
@@ -10,9 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var screenWidth = 0
+    var screenHeight = 0
+    var grass = UILabel()
+    var scoreBox = UILabel()
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let screenBounds: CGRect = UIScreen.main.bounds
+        screenWidth = Int(screenBounds.width)
+        screenHeight = Int(screenBounds.height)
+        
+        //scoreBox
+        scoreBox.frame = CGRect(x: 20, y: 20, width: screenWidth - 40, height: screenHeight/10)
+        scoreBox.text = String(score)
+        
+        view.addSubview(scoreBox)
+        
+        //grass background
+        grass.frame = CGRect(x: 20, y: screenHeight / 10 + 20, width: screenWidth - 40, height: screenHeight - (screenHeight / 10) - 40)
+        grass.backgroundColor = .green
+        
+        view.addSubview(grass)
+        
+        self.view = view
     }
 
 
