a583bde1088fca805f14c622ad40a95738e8aad1
Ella Lindstrom <ella.lindstroem.548@k12.friscoisd.org>
2020-10-19 09:12:52 -0500

    ends game after 10 moles & displays score fixed timer issue

--- WhackAMole/ViewController.swift
+++ WhackAMole/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UIViewController {
     var score = 0
     var mole = UIButton()
     var timer = Timer()
+    var numMoles = 0
+    var endGameReport = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,7 +29,7 @@ class ViewController: UIViewController {
         
         //scoreBox
         scoreBox.frame = CGRect(x: 20, y: 20, width: screenWidth - 40, height: screenHeight/10)
-        scoreBox.text = String(score)
+        scoreBox.text = "Score: \(score)"
         
         view.addSubview(scoreBox)
         
@@ -62,7 +64,7 @@ class ViewController: UIViewController {
         
         //scoreBox
         scoreBox.frame = CGRect(x: 20, y: 20, width: screenWidth - 40, height: screenHeight/10)
-        scoreBox.text = String(score)
+        scoreBox.text = "Score: \(score)"
         
         //grass background
         grass.frame = CGRect(x: 20, y: screenHeight / 10 + 20, width: screenWidth - 40, height: screenHeight - (screenHeight / 10) - 40)
@@ -72,17 +74,22 @@ class ViewController: UIViewController {
     @objc func makeNewButton(diameter: Int) {
         mole.removeFromSuperview()
         
+        if numMoles >= 10 {
+            endGame()
+        } else {
         
-        //new button
-        let randomDiameter = Int.random(in: 10...50)
-        let maxXRight = (screenWidth - 20) - randomDiameter
-        let maxYBottom = (screenHeight - 20) - randomDiameter
-        let randomX = Int.random(in: 20...maxXRight)
-        let randomY = Int.random(in: (screenHeight / 10 + 20)...maxYBottom)
+            //new button
+            let randomDiameter = Int.random(in: 10...50)
+            let maxXRight = (screenWidth - 20) - randomDiameter
+            let maxYBottom = (screenHeight - 20) - randomDiameter
+            let randomX = Int.random(in: 20...maxXRight)
+            let randomY = Int.random(in: (screenHeight / 10 + 20)...maxYBottom)
         
-        mole.frame = CGRect(x: randomX, y: randomY, width: randomDiameter, height: randomDiameter)
-        mole.layer.cornerRadius = CGFloat(randomDiameter / 2)
-        view.addSubview(mole)
+            mole.frame = CGRect(x: randomX, y: randomY, width: randomDiameter, height: randomDiameter)
+            mole.layer.cornerRadius = CGFloat(randomDiameter / 2)
+            numMoles += 1
+            view.addSubview(mole)
+        }
     }
     
     @objc func whacked(_ sender:UIButton!) {
@@ -100,10 +107,69 @@ class ViewController: UIViewController {
             score += 1
         }
         
-        scoreBox.text = String(score)
+        scoreBox.text = "Score: \(score)"
     }
     @objc func timerWentOff() {
         makeNewButton(diameter: Int.random(in: 10...50))
+    }
+    
+    @objc func endGame() {//puts the button thing
+        numMoles = 0
+        timer.invalidate()
+        scoreBox.removeFromSuperview()
+        
+        //the button to click that makes the game start over
+        endGameReport.frame = CGRect(x: 20, y: screenHeight / 10 + 20, width: screenWidth - 40, height: screenHeight - (screenHeight / 10) - 40)
+        endGameReport.addTarget(self, action: #selector(startOver(_:)), for: .touchUpInside)
+        endGameReport.setTitleColor(.black, for: .normal)
+        endGameReport.setTitle("Score: \(score)", for: .normal)
+        view.addSubview(endGameReport)
+        
+        
+        self.view = view
+    }
+    
+    @objc func startOver(_ sender:UIButton!) {//sets game up to how it was
+        endGameReport.removeFromSuperview()
+        endGameReport.frame = CGRect(x: screenWidth, y: 0, width: 1, height: 1)
+        view.addSubview(endGameReport)
+        view.addSubview(scoreBox)
+        self.view = view
+        
+        scoreBox.frame = CGRect(x: 20, y: 20, width: screenWidth - 40, height: screenHeight/10)
+        score = 0
+        
+        //scoreBox.frame = CGRect(x: 20, y: 20, width: screenWidth - 40, height: screenHeight/10)
+        
+        game()
+    }
+    
+    @objc func game() {//initializing code to set game up again
+        //scoreBox
+        scoreBox.frame = CGRect(x: 20, y: 20, width: screenWidth - 40, height: screenHeight/10)
+        scoreBox.textAlignment = NSTextAlignment.left
+        scoreBox.text = "Score: \(score)"
+        
+        view.addSubview(scoreBox)
+        
+        //grass background
+        grass.frame = CGRect(x: 20, y: screenHeight / 10 + 20, width: screenWidth - 40, height: screenHeight - (screenHeight / 10) - 40)
+        grass.backgroundColor = .green
+        
+        view.addSubview(grass)
+        
+        //first mole
+        mole.frame = CGRect(x: screenWidth / 2, y: screenHeight / 2, width: 40, height: 40)
+        mole.layer.cornerRadius = 20
+        mole.backgroundColor = .brown
+        mole.addTarget(self, action: #selector(whacked(_:)), for: .touchUpInside)
+        
+        view.addSubview(mole)
+        
+        self.view = view
+        
+        //set countdown for mole
+        timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(timerWentOff), userInfo: nil, repeats: true)
     }
 
 }
